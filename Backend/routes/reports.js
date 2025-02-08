89566525998587482a10b9c9cfbfc7d960e5a20f
mishrabhi <asmyselfabhishek00@gmail.com>
2025-02-09 05:20:48 +0530

    fixed:errors

--- Backend/routes/reports.js
+++ Backend/routes/reports.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   try {
     const reports = await creditReport.find();
-    res.status(200), json(reports);
+    res.status(200).json(reports);
   } catch (error) {
     res.status(500), json({ message: "Error fetching reports" });
   }
